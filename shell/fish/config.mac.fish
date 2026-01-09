d125259ab8b8983a17944a519e2ec13e56024ab5
Kent Huang <kent@infuseai.io>
2026-01-09 20:28:13 +0800

    Revert "update fish config"
    This reverts commit dd210c3457b9ecaeffcc481bda21e3a340e7f002.

--- shell/fish/config.mac.fish
+++ shell/fish/config.mac.fish
@@ -4,32 +4,30 @@
 # Init configuration
 set fish_greeting ""
 
-test -f ~/.config/fish/k8s.fish && source ~/.config/fish/k8s.fish > /dev/null
-test -f ~/.config/fish/secrets.fish && source ~/.config/fish/secrets.fish >/dev/null
+source ~/.config/fish/k8s.fish > /dev/null
+source ~/.config/fish/git-together.fish > /dev/null
 
 # Set Go Path
 set -x GOPATH ~/goenv/
 set -g -x PATH $PATH $GOPATH/bin
-set -g -x PATH $PATH ~/bin
+set -g -x PATH $PATH ~/.infuseai/bin
 
 # Powerline-shell
 function fish_prompt
-    ~/GitHub/powerline-go/powerline-go -error $status -shell bare --modules "venv,user,ssh,cwd,perms,git,exit,root" -cwd-max-depth 3
+    #~/.powerline-shell.py $status --shell bare ^/dev/null
+    ~/GitHub/powerline-go/powerline-go -error $status -shell bare --modules "kube,venv,user,ssh,cwd,perms,git,node,exit,root" -cwd-max-depth 3
 end
 
 function fish_right_prompt; end
 
+# set fish_function_path $fish_function_path "/usr/local/lib/python2.7/site-packages/powerline/bindings/fish/"
+# powerline-setup
+
 # NVM Fish Wrapper
-export NVM_DIR="$HOME/.nvm"
+source ~/.config/fish/nvm-wrapper/nvm.fish
 
 # Visual Studio Code
 function code
     set -x VSCODE_CWD $PWD
     open -n -b "com.microsoft.VSCode" --args $argv
 end
-
-alias python="python3"
-fish_add_path /opt/homebrew/bin
-fish_prompt > /dev/null
-
-pyenv init - fish | source
