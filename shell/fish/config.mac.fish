dd210c3457b9ecaeffcc481bda21e3a340e7f002
Kent Huang <kent@infuseai.io>
2026-01-09 20:18:45 +0800

    update fish config
    Signed-off-by: Kent Huang <kent@infuseai.io>

--- shell/fish/config.mac.fish
+++ shell/fish/config.mac.fish
@@ -4,30 +4,32 @@
 # Init configuration
 set fish_greeting ""
 
-source ~/.config/fish/k8s.fish > /dev/null
-source ~/.config/fish/git-together.fish > /dev/null
+test -f ~/.config/fish/k8s.fish && source ~/.config/fish/k8s.fish > /dev/null
+test -f ~/.config/fish/secrets.fish && source ~/.config/fish/secrets.fish >/dev/null
 
 # Set Go Path
 set -x GOPATH ~/goenv/
 set -g -x PATH $PATH $GOPATH/bin
-set -g -x PATH $PATH ~/.infuseai/bin
+set -g -x PATH $PATH ~/bin
 
 # Powerline-shell
 function fish_prompt
-    #~/.powerline-shell.py $status --shell bare ^/dev/null
-    ~/GitHub/powerline-go/powerline-go -error $status -shell bare --modules "kube,venv,user,ssh,cwd,perms,git,node,exit,root" -cwd-max-depth 3
+    ~/GitHub/powerline-go/powerline-go -error $status -shell bare --modules "venv,user,ssh,cwd,perms,git,exit,root" -cwd-max-depth 3
 end
 
 function fish_right_prompt; end
 
-# set fish_function_path $fish_function_path "/usr/local/lib/python2.7/site-packages/powerline/bindings/fish/"
-# powerline-setup
-
 # NVM Fish Wrapper
-source ~/.config/fish/nvm-wrapper/nvm.fish
+export NVM_DIR="$HOME/.nvm"
 
 # Visual Studio Code
 function code
     set -x VSCODE_CWD $PWD
     open -n -b "com.microsoft.VSCode" --args $argv
 end
+
+alias python="python3"
+fish_add_path /opt/homebrew/bin
+fish_prompt > /dev/null
+
+pyenv init - fish | source
